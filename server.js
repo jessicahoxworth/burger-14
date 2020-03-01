d8837f1b3c7dc1e141721ac6c77983684f867087
Jessica Hoxworth <jessicahoxworth@gmail.com>
2020-03-01 11:40:12 -0600

    ignore

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
