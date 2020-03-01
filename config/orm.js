98593093793b2d618d764c526dc980746e8fddfe
Jessica Hoxworth <jessicahoxworth@gmail.com>
2020-03-01 11:14:42 -0600

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -19,11 +19,11 @@ function objToSql(ob) {
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
         var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value - " ' " + value + " ' ";
-            }
-        }
+        // if (Object.hasOwnProperty.call(ob, key)) {
+        //     if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        //         value - " ' " + value + " ' ";
+        //     }
+        // }
         arr.push(key + "=" + ob[key]);
     }
     return arr.toString();
